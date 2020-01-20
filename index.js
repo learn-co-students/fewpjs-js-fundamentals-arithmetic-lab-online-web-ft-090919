cfe6421d141b20fb8ca2879c1742d2ffb6bcb89d
Gabriel Kutik <gabrielkutik@yahoo.com>
2020-01-20 11:14:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 1;
+let num2  = 62;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.random() + 1;
+
+let mod = 11 % 7;
+
+let max = Math.max(1, 20);
+// Write your code here`
